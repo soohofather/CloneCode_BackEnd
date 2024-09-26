7645f59d3a852120abf35d4b1903b3783e38f0a9
soohofather <enthchal@gmail.com>
2024-09-26 15:40:09 +0900

    섹션 7 : JWT와 @PreAuthorize 이용하기

--- mallBackEnd/src/main/java/org/zerok/mall/config/CustomSecurityConfig.java
+++ mallBackEnd/src/main/java/org/zerok/mall/config/CustomSecurityConfig.java
@@ -5,20 +5,24 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.zerok.mall.security.filter.JwtCheckFilter;
 import org.zerok.mall.security.handler.ApiLoginFailHandler;
 import org.zerok.mall.security.handler.ApiLoginSuccessHandler;
 
 @Configuration
 @Log4j2
 @RequiredArgsConstructor
+@EnableMethodSecurity
 public class CustomSecurityConfig {
 
     @Bean
@@ -42,6 +46,8 @@ public class CustomSecurityConfig {
                 config.successHandler(new ApiLoginSuccessHandler());
                 config.failureHandler(new ApiLoginFailHandler());
         });
+
+        http.addFilterBefore(new JwtCheckFilter(), UsernamePasswordAuthenticationFilter.class);
 
 
         return http.build();
---
+++ mallBackEnd/src/main/java/org/zerok/mall/security/filter/JwtCheckFilter.java
@@ -0,0 +1,96 @@
+package org.zerok.mall.security.filter;
+
+
+import com.google.gson.Gson;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.zerok.mall.dto.MemberDto;
+import org.zerok.mall.util.JwtUtil;
+
+@Log4j2
+public class JwtCheckFilter extends OncePerRequestFilter {
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+
+        // true == not checking
+
+        String path = request.getRequestURI();
+        log.info("---------------Check uri : {}",path);
+
+        if(path.startsWith("/api/member/")){
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
+
+        log.info("--------------------------");
+
+        log.info("--------------------------");
+
+        log.info("--------------------------");
+
+        String authHeaderStr = request.getHeader("Authorization");
+
+        try {
+            //Bearer accestoken...
+            String accessToken = authHeaderStr.substring(7);
+            Map<String, Object> claims = JwtUtil.validateToken(accessToken);
+
+            log.info("JWT claims: " + claims);
+
+            //filterChain.doFilter(request, response); //이하 추가
+
+            String email = (String) claims.get("email");
+            String pw = (String) claims.get("pw");
+            String nickname = (String) claims.get("nickname");
+            Boolean social = (Boolean) claims.get("social");
+            List<String> roleNames = (List<String>) claims.get("roleNames");
+
+            MemberDto memberDTO = new MemberDto(email, pw, nickname, social.booleanValue(), roleNames);
+
+            log.info("-----------------------------------");
+            log.info(memberDTO);
+            log.info(memberDTO.getAuthorities());
+
+            UsernamePasswordAuthenticationToken authenticationToken
+                    = new UsernamePasswordAuthenticationToken(memberDTO, pw, memberDTO.getAuthorities());
+
+            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+
+            filterChain.doFilter(request, response);
+
+        }catch(Exception e){
+
+            log.error("JWT Check Error..............");
+            log.error(e.getMessage());
+
+            Gson gson = new Gson();
+            String msg = gson.toJson(Map.of("error", "ERROR_ACCESS_TOKEN"));
+
+            response.setContentType("application/json");
+            PrintWriter printWriter = response.getWriter();
+            printWriter.println(msg);
+            printWriter.close();
+        }
+    }
+}
